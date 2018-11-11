c5ca799b24e44c6850c94bbb051ff11041b100b7
Jenni <mmunozjennifer@gmail.com>
2018-11-11 15:43:09 -0500

    Add files via upload

---
+++ Bloomington Thrift Shop/main.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+    var elems2 = document.querySelectorAll('.carousel');
+    var instances2 = M.Carousel.init(elems2);
+});
